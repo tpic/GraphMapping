598f59dafd5df3ab0a89191e980694f7ec799c2f
Thomas Picard <thomas.picard54@sfr.fr>
2013-03-25 21:17:58 +0100

    Structure up, ajout notoin HashMap

--- src/Graph.java
+++ src/Graph.java
@@ -8,20 +8,20 @@ public class Graph {
 		this.graph = new ArrayList<Noeud>();
 	}
 	
-	public void addRelation(Noeud nTo, Relation r, Noeud nFrom){
-		nTo.addFlux(r);
+	public void addRelation(Noeud nFrom, Relation r, Noeud nTo){
+		nFrom.addFlux(r);
 		Sens sens = r.getSens();
 		if(r.getSens().toString().equalsIgnoreCase(Sens.IN.toString())){
 			sens = Sens.OUT;
 		} else if(r.getSens().toString().equalsIgnoreCase(Sens.OUT.toString())){
 			sens = Sens.IN;
 		}
-		Relation reverse = new Relation(r.getName(), nFrom, nTo, sens);
+		Relation reverse = new Relation(r.getName(), nFrom, sens);
 		reverse.addAttribut(r.getMapAttribut());
-		nFrom.addFlux(reverse);
+		nTo.addFlux(reverse);
 	}
 	
-	public Noeud addNoeud(String name){
+	public Noeud addOrGetNoeud(String name){
 		if(noeudPresent(name)){
 			return getNoeud(name);
 		}
