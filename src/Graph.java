863c2a8535579f6ff51f18552f7a79153b22b63b
Thomas Picard <thomas.picard54@sfr.fr>
2013-03-26 10:27:58 +0100

    Fin modification structure

--- src/Graph.java
+++ src/Graph.java
@@ -1,28 +1,27 @@
 import java.util.ArrayList;
 
-
 public class Graph {
 	private ArrayList<Noeud> graph;
-	
-	public Graph(){
+
+	public Graph() {
 		this.graph = new ArrayList<Noeud>();
 	}
-	
-	public void addRelation(Noeud nFrom, Relation r, Noeud nTo){
+
+	public void addRelation(Noeud nFrom, Relation r, Noeud nTo) {
 		nFrom.addFlux(r);
 		Sens sens = r.getSens();
-		if(r.getSens().toString().equalsIgnoreCase(Sens.IN.toString())){
+		if (r.getSens().toString().equalsIgnoreCase(Sens.IN.toString())) {
 			sens = Sens.OUT;
-		} else if(r.getSens().toString().equalsIgnoreCase(Sens.OUT.toString())){
+		} else if (r.getSens().toString().equalsIgnoreCase(Sens.OUT.toString())) {
 			sens = Sens.IN;
 		}
 		Relation reverse = new Relation(r.getName(), nFrom, sens);
 		reverse.addAttribut(r.getMapAttribut());
 		nTo.addFlux(reverse);
 	}
-	
-	public Noeud addOrGetNoeud(String name){
-		if(noeudPresent(name)){
+
+	public Noeud addOrGetNoeud(String name) {
+		if (noeudPresent(name)) {
 			return getNoeud(name);
 		}
 		Noeud n = new Noeud(name);
@@ -30,31 +29,31 @@ public class Graph {
 		return n;
 	}
 
-	public Noeud getNoeud(String name){
-		for(Noeud n : graph){
-			if(n.getName().equalsIgnoreCase((name))){
+	public Noeud getNoeud(String name) {
+		for (Noeud n : graph) {
+			if (n.getName().equalsIgnoreCase((name))) {
 				return n;
 			}
 		}
 		return null;
 	}
-	
-	public boolean noeudPresent(Noeud n){
+
+	public boolean noeudPresent(Noeud n) {
 		return graph.contains(n);
 	}
-	
-	public boolean noeudPresent(String name){
-		for(Noeud n : graph){
-			if(n.getName().equalsIgnoreCase((name))){
+
+	public boolean noeudPresent(String name) {
+		for (Noeud n : graph) {
+			if (n.getName().equalsIgnoreCase((name))) {
 				return true;
 			}
 		}
 		return false;
 	}
-	
-	public String toString(){
+
+	public String toString() {
 		StringBuffer buff = new StringBuffer();
-		for(Noeud n : graph){
+		for (Noeud n : graph) {
 			buff.append(n);
 		}
 		return buff.toString();
--- src/Noeud.java
+++ src/Noeud.java
@@ -7,14 +7,17 @@ public class Noeud {
 	// ____________________"share" -> Relation ] >
 	// ____ = < "out" --> [ "friends" -> [Relation1, Relation2] >
 	// _____= < "inout"> [ "friends" -> [Relation2] ] >
-	private HashMap<String, ArrayList<HashMap<String,ArrayList<Relation>>>> flux;
+	private HashMap<String, ArrayList<HashMap<String, ArrayList<Relation>>>> flux;
 
 	public Noeud(String name) {
 		this.name = name;
-		this.flux = new HashMap<String, ArrayList<HashMap<String,ArrayList<Relation>>>>();
-		this.flux.put("IN", new ArrayList<HashMap<String,ArrayList<Relation>>>());
-		this.flux.put("OUT", new ArrayList<HashMap<String,ArrayList<Relation>>>());
-		this.flux.put("INOUT", new ArrayList<HashMap<String,ArrayList<Relation>>>());
+		this.flux = new HashMap<String, ArrayList<HashMap<String, ArrayList<Relation>>>>();
+		this.flux.put("IN",
+				new ArrayList<HashMap<String, ArrayList<Relation>>>());
+		this.flux.put("OUT",
+				new ArrayList<HashMap<String, ArrayList<Relation>>>());
+		this.flux.put("INOUT",
+				new ArrayList<HashMap<String, ArrayList<Relation>>>());
 	}
 
 	public String getName() {
@@ -23,9 +26,10 @@ public class Noeud {
 
 	public void addFlux(Relation r) {
 		boolean add = false;
-		for(HashMap<String,ArrayList<Relation>> aList : this.flux.get(r.getSensString())){
-			if(aList.containsKey(r.getName())){
-				//TODO verif si ! doublon
+		for (HashMap<String, ArrayList<Relation>> aList : this.flux.get(r
+				.getSensString())) {
+			if (aList.containsKey(r.getName())) {
+				// TODO verif si ! doublon
 				aList.get(r.getName()).add(r);
 				add = true;
 				break;
@@ -36,29 +40,29 @@ public class Noeud {
 			add = true;
 			break;
 		}
-		if(!add){			
-			//Quand la hashmap In, Out ou Inout est vide
+		if (!add) {
+			// Quand la hashmap In, Out ou Inout est vide
 			ArrayList<Relation> l = new ArrayList<Relation>();
 			l.add(r);
 			HashMap<String, ArrayList<Relation>> hm = new HashMap<String, ArrayList<Relation>>();
 			hm.put(r.getName(), l);
 			this.flux.get(r.getSensString()).add(hm);
 		}
-	}	
-	
-	public boolean equals(Noeud n){
+	}
+
+	public boolean equals(Noeud n) {
 		return this.name.equals(n.getName());
 	}
-	
-	public String toString(){
-		StringBuffer buff = new StringBuffer(this.name+"\n");		
-		for(String s : flux.keySet()){
-			buff.append("\t "+s+"\n");
-			for(HashMap<String, ArrayList<Relation>> hm : this.flux.get(s)){
-				for(String titleRelation : hm.keySet()){
-					buff.append("\t\t "+titleRelation+"\n");
-					for(Relation r : hm.get(titleRelation)){
-						buff.append("\t\t\t "+r+"\n");
+
+	public String toString() {
+		StringBuffer buff = new StringBuffer(this.name + "\n");
+		for (String s : flux.keySet()) {
+			buff.append("\t " + s + "\n");
+			for (HashMap<String, ArrayList<Relation>> hm : this.flux.get(s)) {
+				for (String titleRelation : hm.keySet()) {
+					buff.append("\t\t " + titleRelation + "\n");
+					for (Relation r : hm.get(titleRelation)) {
+						buff.append("\t\t\t " + r + "\n");
 					}
 				}
 			}
