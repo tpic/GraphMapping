7872ee64af4df78ecc8171f8808d1ceeeaf1a06b
Thomas Picard <thomas.picard54@sfr.fr>
2013-03-28 21:20:21 +0100

    Fonction de parcours de graph

--- src/Noeud.java
+++ src/Noeud.java
@@ -12,12 +12,9 @@ public class Noeud {
 	public Noeud(String name) {
 		this.name = name;
 		this.flux = new HashMap<String, ArrayList<HashMap<String, ArrayList<Relation>>>>();
-		this.flux.put("IN",
-				new ArrayList<HashMap<String, ArrayList<Relation>>>());
-		this.flux.put("OUT",
-				new ArrayList<HashMap<String, ArrayList<Relation>>>());
-		this.flux.put("INOUT",
-				new ArrayList<HashMap<String, ArrayList<Relation>>>());
+		this.flux.put("IN", new ArrayList<HashMap<String, ArrayList<Relation>>>());
+		this.flux.put("OUT", new ArrayList<HashMap<String, ArrayList<Relation>>>());
+		this.flux.put("INOUT", new ArrayList<HashMap<String, ArrayList<Relation>>>());
 	}
 
 	public String getName() {
@@ -27,8 +24,7 @@ public class Noeud {
 	public void addFlux(Relation r) {
 		if (!verifDoublon(r)) {
 			boolean add = false;
-			for (HashMap<String, ArrayList<Relation>> aList : this.flux.get(r
-					.getSensString())) {
+			for (HashMap<String, ArrayList<Relation>> aList : this.flux.get(r.getSensString())) {
 				if (aList.containsKey(r.getName())) {
 					aList.get(r.getName()).add(r);
 					add = true;
@@ -52,8 +48,7 @@ public class Noeud {
 	}
 
 	public boolean verifDoublon(Relation r) {
-		for (HashMap<String, ArrayList<Relation>> hm : this.flux.get(r
-				.getSens().toString())) {
+		for (HashMap<String, ArrayList<Relation>> hm : this.flux.get(r.getSens().toString())) {
 			for (String titleRelation : hm.keySet()) {
 				if (titleRelation.equalsIgnoreCase(r.getName())) {
 					for (Relation relation : hm.get(titleRelation)) {
@@ -66,6 +61,53 @@ public class Noeud {
 			}
 		}
 		return false;
+	}
+
+	public ArrayList<Relation> getFluxEntrant() {
+		ArrayList<Relation> listRelation = new ArrayList<Relation>();
+		for (HashMap<String, ArrayList<Relation>> hm : this.flux.get(Sens.IN.toString())) {
+			for (String titleRelation : hm.keySet()) {
+				listRelation.addAll(hm.get(titleRelation));
+			}
+		}
+		// Ajout des inouts
+		listRelation.addAll(this.getFluxDouble());
+		return listRelation;
+	}
+
+	public ArrayList<Relation> getFluxSortant() {
+		ArrayList<Relation> listRelation = new ArrayList<Relation>();
+		for (HashMap<String, ArrayList<Relation>> hm : this.flux.get(Sens.OUT.toString())) {
+			for (String titleRelation : hm.keySet()) {
+				listRelation.addAll(hm.get(titleRelation));
+			}
+		}
+		// Ajout des inouts
+		listRelation.addAll(this.getFluxDouble());
+		return listRelation;
+	}
+
+	public ArrayList<Relation> getFluxDouble() {
+		ArrayList<Relation> listRelation = new ArrayList<Relation>();
+		for (HashMap<String, ArrayList<Relation>> hm : this.flux.get(Sens.INOUT.toString())) {
+			for (String titleRelation : hm.keySet()) {
+				listRelation.addAll(hm.get(titleRelation));
+			}
+		}
+		return listRelation;
+	}
+
+	public ArrayList<Relation> getRelation(String sens, String nomRelation) {
+		ArrayList<Relation> listRelation = new ArrayList<Relation>();
+		for (HashMap<String, ArrayList<Relation>> hm : this.flux.get(sens)) {
+			for (String titleRelation : hm.keySet()) {
+				if (titleRelation.equalsIgnoreCase(nomRelation)) {
+					listRelation.addAll(hm.get(titleRelation));
+					// Voir si on peut pas return tout de suite
+				}
+			}
+		}
+		return listRelation;
 	}
 
 	public boolean equals(Noeud n) {
--- src/Tests.java
+++ src/Tests.java
@@ -1,13 +1,13 @@
-
 public class Tests {
 
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		Graph g = new Graph();
 
 		Parser p = new Parser(g);
 		p.verifFichier("donnees.txt");
-		
+
 		System.out.println(g);
+		Recherche.parcours(g);
 	}
-	
+
 }
